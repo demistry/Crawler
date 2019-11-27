de3b486857bd8cd42dee49e30ac2d30aadba3671
David Ilenwabor <dave11ilen@gmail.com>
2019-11-27 07:30:31 +0100

    Formatting and printing of all href tags in a page

--- Crawler.go
+++ Crawler.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/jackdanger/collectlinks"
 )
 
 func main() {
@@ -16,8 +18,17 @@ func main() {
 		fmt.Println("Please enter a start web address...")
 		os.Exit(1)
 	}
-	retrivedBody := retrieveURL(args[0])
-	fmt.Println("Retrieved body is ", retrivedBody)
+	resp, err := http.Get(args[0])
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	links := collectlinks.All(resp.Body)
+
+	for _, link := range links {
+		fmt.Println(link)
+	}
 }
 
 func retrieveURL(url string) string {
