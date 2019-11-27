89993b4083dcfb93861fd12e3c836e5744201a62
David Ilenwabor <dave11ilen@gmail.com>
2019-11-27 08:05:27 +0100

    added parsing of url strings from html

--- Crawler.go
+++ Crawler.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/jackdanger/collectlinks"
@@ -18,7 +20,28 @@ func main() {
 		fmt.Println("Please enter a start web address...")
 		os.Exit(1)
 	}
-	resp, err := http.Get(args[0])
+
+	//creating a channel to hold links as they come in
+	queue := make(chan string)
+
+	go func() {
+		queue <- args[0]
+	}()
+
+	for uri := range queue {
+		enqueue(uri, queue)
+	}
+
+}
+
+func enqueue(uri string, queue chan string) {
+	fmt.Println("Fetching uri...")
+
+	tlsConfig := &tls.Config{InsecureSkipVerify: true} //skips ssl configured sites
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := http.Client{Transport: transport}
+
+	resp, err := client.Get(uri)
 	if err != nil {
 		return
 	}
@@ -27,8 +50,26 @@ func main() {
 	links := collectlinks.All(resp.Body)
 
 	for _, link := range links {
-		fmt.Println(link)
+		absolute := fixURL(link, uri)
+		if uri != "" {
+			go func() { queue <- absolute }()
+		}
+
 	}
+}
+
+func fixURL(href, base string) string {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+
+	baseURL, err := url.Parse(base)
+	if err != nil {
+		return ""
+	}
+	uri = baseURL.ResolveReference(uri)
+	return uri.String()
 }
 
 func retrieveURL(url string) string {
